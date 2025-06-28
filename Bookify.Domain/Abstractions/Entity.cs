9e5cee446a6a656e1f551b553c96ad6edcd91d63
Wardenbug <danylo.storozhuk.ua@gmail.com>
2025-06-28 13:27:15 +0200

    add domain layer

---
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,28 @@
+﻿namespace Bookify.Domain.Abstractions;
+
+public abstract class Entity
+{
+    private readonly List<IDomainEvent> _domainEvent = new();
+
+
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+    public Guid Id { get; init; }
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return _domainEvent.ToList();
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvent.Clear();
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvent.Add(domainEvent);
+    }
+}
